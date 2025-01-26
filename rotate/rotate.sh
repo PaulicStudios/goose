745ddf93bb3dc969944bfa631f3cdd7782a3dd6e
pgrossma <pgrossma@student.42heilbronn.de>
2025-01-26 17:04:22 +0100

    more rotating

--- rotate/rotate.sh
+++ rotate/rotate.sh
@@ -11,4 +11,20 @@ while true; do
     ~/.fb-rotate -d -1 -r 270
     sleep $sleep
     ~/.fb-rotate -d -1 -r 0
+    sleep 5
+    ~/.fb-rotate -d -1 -r 90
+    sleep 2
+    ~/.fb-rotate -d -1 -r 180
+    sleep 2
+    ~/.fb-rotate -d -1 -r 270
+    sleep 2
+    ~/.fb-rotate -d -1 -r 0
+    sleep 2
+    ~/.fb-rotate -d -1 -r 90
+    sleep 2
+    ~/.fb-rotate -d -1 -r 180
+    sleep 2
+    ~/.fb-rotate -d -1 -r 270
+    sleep 2
+    ~/.fb-rotate -d -1 -r 0
 done
