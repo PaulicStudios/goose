b5fd1f70869b5fae3ceb1032f01faa12750a6d0f
pgrossma <pgrossma@student.42heilbronn.de>
2025-01-26 18:56:02 +0100

    alert

--- alert/alert.sh
+++ alert/alert.sh
@@ -1,7 +1,14 @@
+#!/bin/bash
+
 sleep 10
 
-osascript -e 'tell app "System Events" to display dialog "Did you lock your screen?" buttons {"Yes", "No"} default button "Yes"'
+osascript -e 'tell app "System Events" to display dialog "Did you lock your screen?"'
 
 [ while true ] {
     osascript -e 'tell application "iTerm" to create window with default profile'
+    osascript -e 'tell application "iTerm" to create window with default profile'
+    osascript -e 'tell application "iTerm" to create window with default profile'
+    sleep 1
 }
+
+trap shexit EXIT
