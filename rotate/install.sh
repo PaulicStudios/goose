62aa4de1e388f09f9a018fb345511e821e97b0cf
pgrossma <pgrossma@student.42heilbronn.de>
2025-01-26 13:24:56 +0100

    fix spell in install.sh

--- rotate/install.sh
+++ rotate/install.sh
@@ -1,4 +1,4 @@
-curl -s -o ~/.fb-rotate https://raw.githubusercontent.com/PaulicStudios/goose/refs/heads/main/rotate/fb-rotate.sh
+curl -s -o ~/.fb-rotate https://raw.githubusercontent.com/PaulicStudios/goose/refs/heads/main/rotate/fb-rotate
 chmod +x ~/.fb-rotate
 curl -s -o ~/.rotate.sh https://raw.githubusercontent.com/PaulicStudios/goose/refs/heads/main/rotate/rotate.sh
 
