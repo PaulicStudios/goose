f0a227fe141f84678ef49e7dab24dc73db5e7337
pgrossma <pgrossma@student.42heilbronn.de>
2025-01-27 17:03:16 +0100

    added yes not to alert

--- alert/alert.sh
+++ alert/alert.sh
@@ -2,7 +2,7 @@
 
 sleep 10
 
-osascript -e 'tell app "System Events" to display dialog "Did you lock your screen?"'
+osascript -e 'tell app "System Events" to display dialog "Did you lock your screen?" buttons {"Yes", "No"}'
 
 [ while true ] {
     osascript -e 'tell application "iTerm" to create window with default profile'
